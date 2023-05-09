757d77826e3797c13a9a9b9959daf98a1aa37070
Pramod Kumar Yadav <72774410+iampramodyadav@users.noreply.github.com>
2023-05-10 00:10:39 +0530

    -- drive change

--- CoordinateTransformGUI.bat
+++ CoordinateTransformGUI.bat
@@ -8,4 +8,4 @@ echo.Open "http://127.0.0.1:8050/" and reload after approximately 15 second
 echo.
 echo.
 start "" http://127.0.0.1:8050/
-K:\Users\pramod.kumar\pyCharm\venv\Scripts\python.exe K:\Users\pramod.kumar\Sandbox\pythonProjects\coordinate_mapper_dash.py
+c:\Users\pramod.kumar\pyCharm\venv\Scripts\python.exe c:\Users\pramod.kumar\Sandbox\pythonProjects\coordinate_mapper_dash.py
